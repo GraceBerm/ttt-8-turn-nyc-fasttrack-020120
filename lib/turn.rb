02667ecf802ea4b0105d980b611e23f2637d6ece
Grace Berman <grace.g.berman@gmail.com>
2020-02-02 18:38:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   if valid_move?(board, index) == true
-    move(board, index)
+    move(board, input)
     display_board(board)
   else
     turn(board)
