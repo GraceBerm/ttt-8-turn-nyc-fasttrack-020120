b064dfc0086bfd1cc2704e291873309a87709442
Grace Berman <grace.g.berman@gmail.com>
2020-02-02 18:43:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,19 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip.to_i
-  input_to_index(input)
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
